f88aeeeea80b36f6081f5911089d8abefe995fde
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-04-18 17:12:31 +0300

    Update

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,13 @@
 import Item from './Item';
 
-export default function List() {
-
-    return (
-        <ul>
-            
-        </ul>
-    );
+export default function List({ tasks }) {
+  return (
+    <ul>
+      {tasks.map(item => (
+        <Item key={item.id} {...item} />
+      ))}
+    </ul>
+  );
 }
+
+
